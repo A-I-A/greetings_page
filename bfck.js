d80a8034245ff9b25683f9a75815e67c237ff87a
A-I-A <box112015@gmail.com>
2020-09-28 08:48:41 +0300

    simple brainfuck interpreter added.

---
+++ bfck.js
@@ -0,0 +1,52 @@
+
+      function translate (input){
+        let initialArray = new Array (1000);
+        initialArray.fill(0);  
+        let carriage = 0;
+        let index =0;
+        let finalString = "";
+        
+        for (; index < input.length; index++ ){
+          switch (input[index]){
+          case "+" : initialArray[carriage] +=1;
+          break;
+          case "-" : initialArray[carriage] -=1;
+          break;
+          case "." : finalString += String.fromCharCode(initialArray[carriage]);
+          break;
+          case "," : initialArray[carriage]=prompt("Введите число",0);
+          break;
+          case ">" : carriage++;
+          break;
+          case "<" : carriage--;
+          break;
+          case "[" : if (initialArray[carriage] == 0)
+                         {index = findRightBrace(input, index) + 1;}
+                     break;
+          case "]" : if (initialArray[carriage] != 0)
+                         {index = findLeftBrace(input, index);}           
+                     break;
+          }
+        }
+       alert(finalString); 
+      } 
+      
+      function findRightBrace (input, index){
+          while (input[index] != "]") {
+              index++;
+          }
+          /*alert(index);*/
+          return index;
+      }
+      
+      function findLeftBrace (input, index){
+          while (input[index] != "[") {
+              index--;
+          }
+          /*alert(index);*/
+          return index;
+      }    
+      
+      translate("++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.");
+
+
